52e9ed200de156f3fd74af4d44dabdc613251adb
Mathias Rav <rav@cs.au.dk>
2010-11-11 14:25:02 +0100

    [Uge 2/imul] Make imul.c support negative numbers and add useful main()

--- Uge 2/imul/imul.c
+++ Uge 2/imul/imul.c
@@ -1,6 +1,5 @@
-/* Integer multiplication. */
-
 #include <stdio.h>
+#include <stdlib.h>
 
 int imul(int x, int y)
 { int p, sub, mask;
@@ -13,8 +12,12 @@ int imul(int x, int y)
     y ^= x;
     x ^= y;
   }
-  while ( x > 0 ) {
-    if (x & mask != 0) {
+  if (x < 0) {
+    x = -x;
+    y = -y;
+  }
+  while ( x != 0 ) {
+    if (x & mask == 0) {
       sub += sub;
       mask |= sub;
       y += y;
@@ -25,9 +28,18 @@ int imul(int x, int y)
   return p;
 }
 
-int main(int argc, char *argv[])
-{ 
-  printf("imul(%d,%d) = %d.\n",0x4000,0x4000, imul(0x4000,0x4000));
+int main(int argc, char *argv[]) { 
+#define usage() {printf("usage: %s x y\n", argv[0]); return 1;}
+  int x,y;
+  char *p;
+  if (argc < 3 || !*argv[1] || !*argv[2]) {usage();}
+  x = strtol(argv[1], &p, 0);
+  if (*p) {usage();}
+  y = strtol(argv[2], &p, 0);
+  if (*p) {usage();}
+  int res = imul(x,y);
+  printf("imul(%d,%d) = %d.\n",x,y,res);
+  return 2*(x*y!=res); // return 2 if res is incorrect
 }
 /* vim:sw=2:ts=2:sts=2:et:
  */
