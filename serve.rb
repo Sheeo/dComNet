63cb758f4633ece6c8725186fa182554ad646f74
Michael Søndergaard <msond@cs.au.dk>
2010-11-26 12:05:43 +0100

    [Generic] Make serve update pdf's only when modified

--- serve.rb
+++ serve.rb
@@ -11,13 +11,15 @@ get '/' do
 end
 
 get '/:week' do |week|
-  tex = "./#{week}/aflevering.tex"
+  tex = File.new("./#{week.chomp}/aflevering.tex")
   if(File.exists?(tex))
-    pdf = "./#{week}/aflevering.pdf"
-    `pdflatex #{tex}` unless File.exist?(pdf)
+    pdf = File.new("./#{week}/aflevering.pdf") if File.exist?("./#{week}/aflevering.pdf")
+    cmd = "pdflatex -halt-on-error -output-directory=\"#{week}\" \"#{tex.path}\""
+    puts "command: #{cmd}"
+    puts `#{cmd}` if pdf.nil? || File::ctime(pdf) < File::ctime(tex)
     [200,
       {'Content-Type' => 'application/pdf'},
-      File.read("./#{week}/aflevering.pdf")]
+      File.read("./#{week}/aflevering.pdf")] if File.exists?("./#{week}/aflevering.pdf")
   else
     404
   end
