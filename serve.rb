b9049d4351cdac993350c4035683ac20af587c9c
Michael Søndergaard <msond@cs.au.dk>
2010-11-26 13:30:38 +0100

    [Generic] Fix serve

--- serve.rb
+++ serve.rb
@@ -2,9 +2,9 @@ require 'rubygems'
 require 'sinatra'
 
 texfiles = ['./Week 1/imul/aflevering.tex',
-            './Week 2/aflevering.tex',
-            './Week 3/aflevering.tex',
-            './Week 4/aflevering.tex']
+  './Week 2/aflevering.tex',
+  './Week 3/aflevering.tex',
+  './Week 4/aflevering.tex']
 
 get '/' do
   haml :index, :locals => {:files => texfiles}
@@ -17,12 +17,15 @@ get '/pdf/*' do
     tex = File.new(tex)
     cmd = "pdflatex -halt-on-error -output-directory=\"#{File.dirname(tex)}\" \"#{tex.path}\""
     pdf = "./#{File.dirname(tex)}/#{File.basename(tex, ".tex")}.pdf"
-    puts "command: #{cmd}"
-    puts `#{cmd}` if File.new(pdf).nil? || File::ctime(pdf) < File::ctime(tex)
+    if File.exists?(pdf) 
+      `#{cmd}` if (File::ctime(pdf) < File::ctime(tex))
+    else
+      `#{cmd}`
+    end
     [200,
       {'Content-Type' => 'application/pdf'},
       File.read(pdf)] if File.exists?(pdf)
   else
-    [404, "Error'd"]
+      [404, "Error'd"]
   end
 end
