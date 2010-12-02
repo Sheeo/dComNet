07f2650f36b4c8c2b98c496c47664fcab7ec64ae
Mathias Rav <rav@cs.au.dk>
2010-12-02 13:46:10 +0100

    [Week 5] Implement argument checking in cfib.c

--- Week 5/cfib.c
+++ Week 5/cfib.c
@@ -3,13 +3,24 @@ int fib(int n) {
 	if (n < 2) return n;
 	return fib(n-2)+fib(n-1);
 }
+#define ret(v) asm("movl " v ", %ebx\n\t" /* return value */ \
+	                 "movl $1, %eax\n\t" /* opcode for exit */ \
+	                 "int $0x80");
 void _start(char *argv0, char *argv1) {
 	int n, val;
 	char *s;
+	if (argv1 == NULL) {
+		printf("Usage: %s <n>\n", argv0);
+		ret("$1");
+		return;
+	}
 	n = strtol(argv1, &s, 0);
+	if (*s) {
+		printf("Usage: %s <n>\n", argv0);
+		ret("$1");
+		return;
+	}
 	val = fib(n);
 	printf("%d\n", val);
-	asm("movl $0, %ebx\n\t" // return value
-	    "movl $1, %eax\n\t" // opcode for exit
-	    "int $0x80");
+	ret("$0");
 }
