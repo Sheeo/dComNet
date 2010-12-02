c6e95b3e5e8740ba4f3257399b110d2f724fe5be
Mathias Rav <rav@cs.au.dk>
2010-12-02 13:42:06 +0100

    [Week 5] Fix line length in afib

--- Week 5/afib.s
+++ Week 5/afib.s
@@ -9,56 +9,56 @@ intfmt: .string "%d\n\0"
 _start:
 	pushl %ebp
 	movl %esp, %ebp
-	subl $8, %esp    # place for local variables
+	subl $8, %esp       # place for local variables
 
-	pushl $0         # strtol(argv[1], &lv0, 0)
+	pushl $0            # strtol(argv[1], &lv0, 0)
 	movl %ebp, %eax
 	subl $4, %eax
-	pushl %eax       # &lv0
-	pushl 12(%ebp)   # argv[0]
-	call strtol      # strtol()
+	pushl %eax          # &lv0
+	pushl 12(%ebp)      # argv[0]
+	call strtol         # strtol()
 	addl $12, %esp
 	movl %eax, -8(%ebp) # store ret val in lv1
 
-	pushl %eax       # push param
-	call fib         # fib()
-	addl $4, %esp    # pop param
+	pushl %eax          # push param
+	call fib            # fib()
+	addl $4, %esp       # pop param
 
 	pushl $0
 	pushl %eax
 	pushl $intfmt
-	call printf      # printf("%d\n", fib(n));
+	call printf         # printf("%d\n", fib(n));
 	addl $12, %esp
 
-	movl $0, %ebx    # return value 0
-	movl $1, %eax    # opcode for exiting
-	int $0x80        # syscall
+	movl $0, %ebx       # return value 0
+	movl $1, %eax       # opcode for exiting
+	int $0x80           # syscall
 
 
-fib:               # fib(int n)
-	pushl %ebp       # add prev stack base to stack
-	movl %esp, %ebp  # set stack base to cur stack pt
+fib:                  # fib(int n)
+	pushl %ebp          # add prev stack base to stack
+	movl %esp, %ebp     # set stack base to cur stack pt
 
-	movl $2, %ebx    # read 2 into ebx
+	movl $2, %ebx       # read 2 into ebx
 	cmpl 8(%ebp), %ebx  # n <=> 2
-	jg ret           # return n if n < 2
+	jg ret              # return n if n < 2
 
 	movl 8(%ebp), %eax
 	decl %eax
-	pushl %eax       # push n-1
-	call fib         # fib(n-1)
-	addl $4, %esp    # pop param
+	pushl %eax          # push n-1
+	call fib            # fib(n-1)
+	addl $4, %esp       # pop param
 
-	pushl %eax       # push ret val
+	pushl %eax          # push ret val
 
 	movl 8(%ebp), %eax
 	subl $2, %eax
-	pushl %eax       # push n-2
-	call fib         # fib(n-2)
-	addl $4, %esp    # pop param
+	pushl %eax          # push n-2
+	call fib            # fib(n-2)
+	addl $4, %esp       # pop param
 
-	addl (%esp), %eax # add fib(n-1) to ret val
-	addl $4, %esp    # pop ret val of fib(n-1)
+	addl (%esp), %eax   # add fib(n-1) to ret val
+	addl $4, %esp       # pop ret val of fib(n-1)
 
 ret:
 	movl %ebp, %esp  # restore esp
