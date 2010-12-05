c40fd430da97732334fd74cf5bb32f393262cc00
Mathias Rav <rav@cs.au.dk>
2010-12-05 15:16:32 +0100

    [Week 5] If no arg passed to cfib.c, read using scanf

--- Week 5/cfib.c
+++ Week 5/cfib.c
@@ -10,15 +10,18 @@ void _start(char *argv0, char *argv1) {
 	int n, val;
 	char *s;
 	if (argv1 == NULL) {
-		printf("Usage: %s <n>\n", argv0);
-		ret("$1");
-		return;
-	}
-	n = strtol(argv1, &s, 0);
-	if (*s) {
-		printf("Usage: %s <n>\n", argv0);
-		ret("$1");
-		return;
+		if (1 != scanf("%d", &n)) {
+			printf("Usage: %s <n>\n", argv0);
+			ret("$1");
+			return;
+		}
+	} else {
+		n = strtol(argv1, &s, 0);
+		if (*s) {
+			printf("Usage: %s <n>\n", argv0);
+			ret("$1");
+			return;
+		}
 	}
 	val = fib(n);
 	printf("%d\n", val);
